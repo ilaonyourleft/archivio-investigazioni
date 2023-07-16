90fdfd36187569ef3779af7389cdd51ecf009021
Ilaria <ilariamalinconico@gmail.com>
2023-07-16 16:49:09 +0200

    update archivio model

--- archivio-be/archivio_be/archivio/models.py
+++ archivio-be/archivio_be/archivio/models.py
@@ -5,7 +5,7 @@ from utente.models import Utente
 
 class Archivio(models.Model):
     nome = models.CharField("nome", max_length=100)
-    fkUtente = models.ForeignKey(Utente, on_delete=models.CASCADE)
+    fkUtente = models.ForeignKey(Utente, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nome
